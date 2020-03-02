1578894c52738ba9adb4b34cabc84eb708d1a4bb
williamjamesmartin <wmartin@stevens.edu>
2020-03-01 19:31:08 -0500

    more formatting

--- src/map.cpp
+++ src/map.cpp
@@ -11,22 +11,16 @@ int main()
 	int width = 50, height = 50;
 
 	for (int i = 0; i < width; i++)
-	{
-		
+	{		
 		for (int j = 0; j < height; j++)
-		{
-			
+		{			
 			if ((rand() % 10 + 1) > 9)
 			{
-
 				arr[i][j] = 1;
-
 			}	
 			else
 			{
-
 				arr[i][j] = 0;
-
 			}
 			
 			std::cout << arr[i][j];	
@@ -35,5 +29,4 @@ int main()
 	}
 
 	return 0;
-
 }
